38c0a4c26db854dd202003feac06d84f0a111133
Candice Bär <candicekbar@gmail.com>
2019-12-20 14:39:30 +0200

    add basic layout and svg components to build the cupcake

--- CupcakeMaker/App.tsx
+++ CupcakeMaker/App.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Svg from './components/svg';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+      <View style={{ flex: 8, flexDirection: 'row' }}>
+        <Svg />
+      </View>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        
+        <View style={{ flex: 1, backgroundColor: '#FF0000' }} />
+        <View style={{ flex: 1, backgroundColor: '#00FF00' }} />
+        <View style={{ flex: 1, backgroundColor: '#FF0000' }} />
+        <View style={{ flex: 1, backgroundColor: '#00FF00' }} />
+        <View style={{ flex: 1, backgroundColor: '#FF0000' }} />
+        <View style={{ flex: 1, backgroundColor: '#00FF00' }} />
+
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333333',
     alignItems: 'center',
     justifyContent: 'center',
   },
